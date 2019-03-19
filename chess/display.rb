7b64424662d9c91e2d0d621d1fb7102a0ff5fe76
ahamacher <andrew.hamacher@gmail.com>
2019-03-18 17:31:44 -0700

    initialize board

--- chess/display.rb
+++ chess/display.rb
@@ -1,7 +1,13 @@
 require "colorize"
+require_relative "board.rb"
+require_relative "cursor.rb"
 
 class Display
-        
+    attr_reader :board
 
+    def initialize()
+        @board = Board.new 
+        @cursor = Cursor.new([0,0],board)
+    end
 
 end
