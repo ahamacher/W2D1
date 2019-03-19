75fb87d3db0d520614897f695e2bbf44bd17f14b
ahamacher <andrew.hamacher@gmail.com>
2019-03-18 17:23:42 -0700

    Refactor board class

--- chess/board.rb
+++ chess/board.rb
@@ -1,31 +1,28 @@
 require_relative "piece.rb"
 require "byebug"
+
 class Board
     attr_accessor :board
     def initialize
         @board = Array.new(8) { Array.new(8, nil) }
-        populate_board
+        self.populate_board
     end
 
     def populate_board
-        @board.each_with_index do |row, i_row|
-            if i_row == 0 || i_row == 7
-                skill_row = [Piece.new, Piece.new, Piece.new, Piece.new, Piece.new, Piece.new, Piece.new, Piece.new]
-                @board[i_row] = skill_row
-            elsif i_row == 1 || i_row == 6 # add row of pawns
-                skill_row = [Piece.new, Piece.new, Piece.new, Piece.new, Piece.new, Piece.new, Piece.new, Piece.new]
-                @board[i_row] = skill_row
+        board.map.with_index do |row, idx|
+            if idx == 0 || idx == 7
+                row.each_with_index { |tile, c_idx| row[c_idx] = Piece.new }
+            elsif idx == 1 || idx == 6
+                row.each_with_index { |tile, c_idx| row[c_idx] = Piece.new }
             end
         end
     end
 
     def move_piece(start_pos,end_pos)
-        # expecting positions to come in array ex. [0,0]
-        s_row, s_col  = start_pos 
-        e_row,e_col = end_pos 
-        if @board[s_row][s_col] && @board[e_row][e_col].nil?
-            @board[e_row][e_col],@board[s_row][s_col] = @board[s_row][s_col],@board[e_row][e_col]
-        elsif @board[s_row][s_col].nil?
+        # expecting positions to come in array ex. [0,0] 
+        if self[start_pos] && self[end_pos].nil?
+            self[end_pos],self[start_pos] = self[start_pos],self[end_pos]
+        elsif self[start_pos].nil?
             raise puts "Non existing piece, try again?"
         else 
             raise puts "Location already contains piece, try again?"
@@ -33,5 +30,14 @@ class Board
         puts "move completed"
     end
 
+    def [](pos)
+        r_idx, c_idx = pos
+        board[r_idx][c_idx]
+    end
+
+    def []=(pos, value)
+        r_idx, c_idx = pos
+        board[r_idx][c_idx] = value
+    end
 
 end
